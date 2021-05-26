e7a7b46a1b60ccae8b5b9f2674b6c3493858efba
Bastian Gebhardt <bastian.gebhardt@idealo.de>
2021-05-26 08:59:57 +0200

    feat: add flags for check period and bluetoothID; add some validation

--- cmd/hp-watch/main.go
+++ cmd/hp-watch/main.go
@@ -14,66 +14,100 @@ import (
     "time"
 )
 
+const programID = "b3507fe0c5c387e072f5f89505c0b03ad35da51f"
+
+// set by compiler
 var version = "0.0.0"
 var buildDate string
 
-var programID = "b3507fe0c5c387e072f5f89505c0b03ad35da51f"
-
-type node struct {
-    name string
-    child []*node
-    parent *node
-    entries []string
+type config struct {
+    versionFlag bool
+    bluetoothID string
+    checkPeriod int
 }
 
-// TODO: remove outdated code (node parsing)
+// TODO: flag validation
 // TODO: fill README.md
 // TODO: add tests
 // TODO: add linter support
-// TODO: add flags for bluetooth address and check period
 // TODO: add check if 'pacmd' is available
 // TODO: add config file support
 // TODO: add log file support
 // TODO: add parser for 'pacmd' output
+// DONE: add flags for bluetooth address and check period
+// DONE: remove outdated code (node parsing)
 
 func main() {
-    versionFlag := flag.Bool("version", false, "Shows the version")
-    flag.Usage = func() {
-        fmt.Printf("Usage of %s ---\n", os.Args[0])
-        flag.PrintDefaults()
-    }
-    flag.Parse()
-
-    if *versionFlag {
-        fmt.Printf("\n    Version   :    %s\n    Build date:    %s\nProgramID :    %s\n", version, buildDate, programID)
-        return
-    }
-
+    // configure logger
     output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.Stamp}
     log.Logger = zerolog.New(output).With().Timestamp().Logger()
     zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
+    cfg := parseCommandLineFlags()
+
+    if cfg.versionFlag {
+        printVersionInfo()
+        os.Exit(0)
+    }
+
     c := cron.New()
-    c.AddFunc("*/1 * * * *", func() {
-        content := getData()
-        found, index := findProfile(content)
+    c.AddFunc(fmt.Sprintf("*/%v * * * *", cfg.checkPeriod), func() {
+        pacmdOut := getPacmdOutput()
+        found, index := findProfile(pacmdOut, cfg.bluetoothID)
         switch {
         case strings.Contains(found,"a2dp_sink"):
             log.Debug().Msg("Profile found, nothing to do")
         case found == "":
-            log.Debug().Msg("Headset 60_AB_D2_29_7E_F8 is not active, nothing to do")
+            log.Debug().Msgf("Headset %s is not active, nothing to do", cfg.bluetoothID)
         case strings.Contains(found, "active profile:"):
-            log.Info().Msg("Headset 60_AB_D2_29_7E_F8 is active, but with wrong profile, will switch to 'a2dp_sink'")
-            setCorrectProfile(index)
+            log.Info().Msgf("Headset %s is active, but with wrong profile, will switch to 'a2dp_sink'", cfg.bluetoothID)
+            setProfileForCard(index, "a2dp_sink")
         }
     })
+    log.Info().Msgf("start watching for profile changes at bluetooth device %s every %vs", cfg.bluetoothID, cfg.checkPeriod)
     c.Run()
 }
 
-func setCorrectProfile(index int) {
+func parseCommandLineFlags() config{
+    var cfg config
+
+    versionFlag := flag.Bool("version", false, "Shows the version")
+    bluetoothIDFlag := flag.String("id", "", "ID of bluetooth device (Format: XX:XX:XX:XX:XX:XX or XX_XX_XX_XX_XX_XX)")
+    checkPeriodFlag := flag.Int("check", 5, "check period in seconds (default: 5)")
+
+    flag.Usage = func() {
+        fmt.Printf("Usage of %s:\n", os.Args[0])
+        flag.PrintDefaults()
+    }
+    flag.Parse()
+
+    // validate flags and put adapted values to config
+    switch {
+    case *checkPeriodFlag<1:
+        log.Warn().Msgf("value of flag '--check' is '%v', but must be in range 1-59; autocorrect to: %v", *checkPeriodFlag, 1)
+        cfg.checkPeriod = 1
+    case *checkPeriodFlag>59:
+        log.Warn().Msgf("value of flag '--check' is '%v', but must be in range 1-59; autocorrect to: %v", *checkPeriodFlag, 59)
+        cfg.checkPeriod = 59
+    }
+
+    // TODO: validation needed
+    cfg.bluetoothID = *bluetoothIDFlag
+
+    cfg.versionFlag = *versionFlag
+
+    return cfg
+}
+
+func printVersionInfo() {
+    fmt.Printf("    Version    :    %s\n    Build date :    %s\n    ProgramID  :    %s\n", version, buildDate, programID)
+}
+
+// runs pacmd command to set profile
+func setProfileForCard(index int, profile string) {
     iStr := strconv.Itoa(index)
-    cmd := exec.Command("pacmd", "set-card-profile", iStr, "a2dp_sink")
-    //cmdReader, _ := cmd.StdoutPipe()
+    cmd := exec.Command("pacmd", "set-card-profile", iStr, profile)
+
     err := cmd.Start()
     if err != nil {
         log.Error().Msg("Cannot start command")
@@ -87,7 +121,7 @@ func setCorrectProfile(index int) {
 }
 
 // runs 'pacmd list-cards' and returns commands output as slices of lines
-func getData() []string {
+func getPacmdOutput() []string {
     var content []string
     cmd := exec.Command("pacmd", "list-cards")
     cmdReader, _ := cmd.StdoutPipe()
@@ -114,7 +148,7 @@ func getData() []string {
 }
 
 // find line with active profile info for headset in commands 'pacmd list-cards' output
-func findProfile(lines []string) (string, int) {
+func findProfile(lines []string, bluetoothID string) (string, int) {
     var err error
     indentation := 0
     searchMode := false
@@ -134,7 +168,7 @@ func findProfile(lines []string) (string, int) {
             continue
         }
         if ! searchMode {
-            if strings.Contains(line,"name: <bluez_card.60_AB_D2_29_7E_F8>") {
+            if strings.Contains(line,fmt.Sprintf("name: <bluez_card.%s>", bluetoothID)) {
                 indentation = getIndentation(line)
                 searchMode = true
                 log.Debug().Msgf("Found headset in line: %s", line)
@@ -156,123 +190,11 @@ func findProfile(lines []string) (string, int) {
     return found, index
 }
 
-func readFile (fileName string) []string {
-    file, err := os.Open(fileName)
-    if err != nil {
-        log.Fatal().Msg("failed to open")
-    }
-    defer func() {
-        err = file.Close()
-        if err != nil {
-            log.Error().Msg("cannot close file handler")
-        }
-    }()
-
-    scanner := bufio.NewScanner(file)
-    scanner.Split(bufio.ScanLines)
-    var text []string
-
-    for scanner.Scan() {
-        text = append(text, scanner.Text())
-    }
-
-    return text
-}
-
-
-func test () {
-    lines := []string {
-        "a",
-        " b",
-        "  c",
-        "   d",
-        "    e",
-        "f",
-        "g",
-        "\th",
-        "\t\ti",
-        "\t\t\tk",
-        "\t\t\t\tl",
-    }
-    parsePAOut(lines)
-}
-
-func parsePAOut(lines []string) {
-    indentation := 0
-    lastLine := "root"
-    root := newNode(lastLine)
-    var keyIndex map[int]*node
-    keyIndex = make(map[int]*node)
-    keyIndex[indentation] = root
-    n := root
-    for _, line := range lines {
-        //replace tabs with 2 spaces
-        line = strings.Replace(line,"\t","  ", -1)
-        newIndentation := getIndentation(line)
-        upOrDown := newIndentation - indentation
-        indentation = newIndentation
-        switch {
-        case upOrDown < 0:
-            // up
-            // TODO: check if nil (root node)
-            n = n.parent
-            n.entries = append(n.entries, strings.Trim(line," "))
-        case upOrDown == 0:
-            n.entries = append(n.entries, strings.Trim(line," "))
-        case upOrDown > 0:
-            // down
-            keyIndex[indentation] = n
-            log.Info().Msgf("goes down: >%v<", line)
-            newNode := newNode(lastLine)
-            newNode.parent = n
-            newNode.entries = append(newNode.entries, strings.Trim(line," "))
-            n.child = append(n.child, newNode)
-            n = newNode
-        }
-        lastLine = line
-        log.Debug().Msgf("Indent: %v", newIndentation)
-    }
-    fmt.Println("Ergebnis")
-    traverse(root)
-}
-
-func newNode(name string) *node {
-    n := node{
-        name: name,
-        child:   nil,
-        parent:  nil,
-        entries: nil,
-    }
-    return &n
-}
-
-func traverse(n *node) {
-    stop := false
-    for ! stop {
-        fmt.Println(n.entries)
-        if n.child != nil {
-            n = n.child[0]
-        } else {
-            stop = true
-        }
-    }
-}
-
-func lineToKeyValue(line string) (string, string) {
-    parts := strings.SplitN(line, ":", 1)
-    c := len(parts)
-    switch {
-    case c == 1:
-        return strings.Trim(parts[0]," "), ""
-    case c == 2:
-        return strings.Trim(parts[0]," "), strings.Trim(parts[1]," ")
-    }
-    return "", ""
-}
-
+// returns count of spaces before text in line begins
+// does not work for tabs
 func getIndentation(l string) int {
     length := len(l)
     trimmed := strings.TrimLeft(l, " ")
-    trimmedLength := len(trimmed)
-    return length - trimmedLength
+
+    return length - len(trimmed)
 }
